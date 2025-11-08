1f54c920e963981ae64427da7df1de4e5d4810d3
Your Name <you@example.com>
2025-11-08 07:40:00 -0500

    main

--- mod/__init__.py
+++ mod/__init__.py
@@ -2,3 +2,9 @@ from .core.mod import Mod
 _mod = Mod()
 for _fn in dir(_mod):
     globals()[_fn] = getattr(_mod, _fn)
+
+def main(self, *args, **kwargs):
+    """
+    Main function to run the mod
+    """
+    _mod.mod('cli')().forward()
